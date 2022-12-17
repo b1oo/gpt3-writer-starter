e1d815c640a6bae5813ef5601355e251774894c1
Sergio <sergiobluez@gmail.com>
2022-12-17 13:26:59 -0800

    Update generate.js
    updated generate.js and checked my .env for my private API key

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,3 +1,10 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
 const basePromptPrefix = "";
 const generateAction = async (req, res) => {
   // Run first prompt
